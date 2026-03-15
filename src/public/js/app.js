233c09f9a4d9e6391bb20f58062231d89db1ee01
swap-dh <boj.jerry@gmail.com>
2026-03-16 00:15:47 +0900

    second

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,16 @@
-alert(1);
+const socket = new WebSocket(`ws://${window.location.host}`);
+socket.addEventListener("open", ()=> {
+    console.log("Connected to server");
+});
+
+socket.addEventListener("message", (message)=> {
+    console.log("Just got this: ", message, "from the server");
+});
+
+socket.addEventListener("close", ()=> {
+    console.log("Closed");
+})
+
+setTimeout(()=> {
+    socket.send("hello from the broswer");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,16 +1,29 @@
 import express from 'express';
-
+import http from 'http';
+import WebSocket from 'ws';
 const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.get('/', (req, res)=> {
     res.render("home");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+wss.on("connection", (socket)=> {
+  console.log("Connected to Browser");
+  socket.on("close", ()=> console.log("DisConnedted"));
+  socket.on("message", (message)=> {
+    console.log(message);    
+  });
+  socket.send("Hello");
+})
+
+server.listen(PORT, ()=>{
+  console.log("Runned");
+})
